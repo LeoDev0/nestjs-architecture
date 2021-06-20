6378b9c7beb92f7e06d1cbbdd51263ffd269aa3e
Leonardo T. Fernandes <leo.80@protonmail.com>
2021-06-20 18:05:08 -0300

    add mocks file and package.json small fix

---
+++ src/users/mocks/data.mock.ts
@@ -0,0 +1,12 @@
+export const allUsersMock = [
+  {
+    id: 1,
+    name: 'leo',
+    email: 'leo.80@protonmail.com',
+  },
+  {
+    id: 2,
+    name: 'marcao',
+    email: 'marcao@gmail.com',
+  },
+];
